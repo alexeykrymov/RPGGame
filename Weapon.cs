37cf2f1d166807e8d69fb52ab89a8781bbb8b677
Alexey Krymov <alexeykrymov@gmail.com>
2023-04-27 22:12:22 +0400

    weapon

--- Weapon.cs
+++ Weapon.cs
@@ -3,24 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static RPGGame.Armor;
 
 namespace RPGGame
 {
-    public enum WeaponTypes // create список с типами Weapon
+    public class Weapon
     {
-        SwordOneHanded,
-        twoHandedSword,
-        OneHandedSpear,
-        TwoHandedSpear,
-        Mace,
-        Hammer,
-        Bow,
-        Crossbow,
-        throwingAx
-    }
-    class Damage
-    {
-        public Damage() { }
-        public Damage(int damage) { }
+        public enum WeaponTypeRange // список дальностит
+        {
+            Short, // короткое
+            Average, // сррденее
+            Far // дальнее
+        }
+        public enum WeaponTypesDamage // список уронов
+        {
+            SlashingDamage, // рубящее
+            StabbingWeapon, // колющее
+            BludgeoningDamage // дробящее
+        }
+        public enum WeaponTypes // create список с типами Weapon
+        {
+            None, // ничего
+            SwordOneHanded, //
+            TwoHandedSword, //
+            OneHandedSpear, //
+            TwoHandedSpear, //
+            Mace,           //
+            Hammer,         //
+            Bow,            //
+            Crossbow,       //
+            ThrowingAx      //
+        }
+        public WeaponTypeRange WeaponRange { get; set; }
+        public WeaponTypes Type { get; } // свойство типа оружия
+        public WeaponTypesDamage TypesDamage { get; private set; } // свойство типа урона
+        public int EquippedWeapon { get; private set; } // одеето ли оружие
+
+
+        public Weapon(WeaponTypeRange weaponRange, WeaponTypes type, WeaponTypesDamage typesDamage) // конструктор, который хранит тип оружия и ее типа урона
+        {
+            WeaponRange = weaponRange; // дальность оружия
+            Type = type; // тип оуржия
+            TypesDamage = typesDamage; // прочность тип урона 
+        }
+       // public Sw
     }
 }
